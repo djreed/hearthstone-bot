f2c739076a80a306e7cd3ccc9f4261bf349a2f53
David James Reed <ddavidreed@gmail.com>
2019-08-15 06:36:14 -0400

    OAuth2 for Blizzard's API

---
+++ blizzard/endpoint.go
@@ -0,0 +1,36 @@
+package blizzard
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+
+	"golang.org/x/oauth2"
+)
+
+// The variables below synchronize access to register an endpoint as
+// broken with the OAuth2 library. Battle.net uses a "broken" implementation
+// of OAuth2.
+var brokenLock sync.Mutex
+var brokenMap = map[string]struct{}{}
+
+// Endpoint returns the endpoint for the given region. This doesn't
+// validate the region name so you must use one that is valid from Battle.net.
+func Endpoint(region string) oauth2.Endpoint {
+	region = strings.ToLower(region)
+	domain := fmt.Sprintf("https://%s.battle.net/", region)
+
+	// Register the broken provider
+	brokenLock.Lock()
+	defer brokenLock.Unlock()
+	if _, ok := brokenMap[domain]; !ok {
+		brokenMap[domain] = struct{}{}
+		oauth2.RegisterBrokenAuthHeaderProvider(domain)
+	}
+
+	// Build the endpoint
+	return oauth2.Endpoint{
+		AuthURL:  domain + "oauth/authorize",
+		TokenURL: domain + "oauth/token",
+	}
+}
---
+++ blizzard/oauth.go
@@ -0,0 +1,25 @@
+package blizzard
+
+import (
+	"context"
+
+	clientAuth "golang.org/x/oauth2/clientcredentials"
+
+	bnet "github.com/djreed/hearthstone-bot/battlenet"
+)
+
+func GetAuthedClient(id, secret string) *bnet.Client {
+	endpoint := Endpoint("us")
+
+	oauthCfg := &clientAuth.Config{
+		ClientID:     id,
+		ClientSecret: secret,
+		TokenURL:     endpoint.TokenURL,
+	}
+
+	ctx := context.TODO()
+	authClient := oauthCfg.Client(ctx)
+	client := bnet.NewClient("us", authClient)
+
+	return client
+}
