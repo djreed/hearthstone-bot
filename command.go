2e4c7dcd74e7e892fa4b33b93d78cf42bddc763b
David James Reed <ddavidreed@gmail.com>
2019-08-15 21:24:07 -0400

    Get rid of boilerplate CLI work

--- command.go
+++ command.go
@@ -5,30 +5,13 @@ import (
 	"os"
 )
 
-func getQuery() string {
-	if len(os.Args) <= 1 {
-		log.Fatal("Invalid number of arguments: must provide card name")
-	}
-	queryArgument := os.Args[1]
-	log.Printf("Query: %s", queryArgument)
-	return queryArgument
-}
-
 func getBlizzardID() string {
 	if BlizzardClientID == "" {
-		log.Print("Blizzard ID not provided in build environment, searching inputs")
+		log.Print("Blizzard ID not provided in build env, searching runtime env")
 
-		if len(os.Args) > 2 {
-			BlizzardClientID = os.Args[2]
-		}
-
-		if BlizzardClientID == "" {
-			log.Print("No Blizzard ID found in arguments, searching environment")
-
-			if BlizzardClientID = os.Getenv("BLIZZARD_ID"); BlizzardClientID == "" {
-				log.Fatal("Failed to find Blizzard ID in environment, exiting")
-				os.Exit(1)
-			}
+		if BlizzardClientID = os.Getenv("BLIZZARD_ID"); BlizzardClientID == "" {
+			log.Fatal("Failed to find Blizzard ID in environment, exiting")
+			os.Exit(1)
 		}
 	}
 
@@ -37,19 +20,11 @@ func getBlizzardID() string {
 
 func getBlizzardSecret() string {
 	if BlizzardClientSecret == "" {
-		log.Print("Blizzard Secret not provided in build environment, searching inputs")
+		log.Print("Blizzard Secret not provided in build env, searching runtime env")
 
-		if len(os.Args) > 2 {
-			BlizzardClientSecret = os.Args[2]
-		}
-
-		if BlizzardClientSecret == "" {
-			log.Print("No Blizzard Secret found in arguments, searching environment")
-
-			if BlizzardClientSecret = os.Getenv("BLIZZARD_SECRET"); BlizzardClientSecret == "" {
-				log.Print("Failed to find Blizzard Secret in environment, exiting")
-				os.Exit(1)
-			}
+		if BlizzardClientSecret = os.Getenv("BLIZZARD_SECRET"); BlizzardClientSecret == "" {
+			log.Print("Failed to find Blizzard Secret in environment, exiting")
+			os.Exit(1)
 		}
 	}
 
@@ -58,19 +33,11 @@ func getBlizzardSecret() string {
 
 func getSlackToken() string {
 	if SlackToken == "" {
-		log.Print("Slack Token not provided in build environment, searching inputs")
+		log.Print("Slack Token not provided in build env, searching runtime env")
 
-		if len(os.Args) > 2 {
-			SlackToken = os.Args[2]
-		}
-
-		if SlackToken == "" {
-			log.Print("No Slack Token found in arguments, searching environment")
-
-			if SlackToken = os.Getenv("SLACK_TOKEN"); SlackToken == "" {
-				log.Print("Failed to find Slack Token in environment, exiting")
-				os.Exit(1)
-			}
+		if SlackToken = os.Getenv("SLACK_TOKEN"); SlackToken == "" {
+			log.Print("Failed to find Slack Token in environment, exiting")
+			os.Exit(1)
 		}
 	}
 
