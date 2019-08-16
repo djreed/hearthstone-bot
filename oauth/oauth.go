440b6ce843712e7ba875b0bf8a110d395884063a
David James Reed <ddavidreed@gmail.com>
2019-08-16 01:04:58 -0400

    Leave work commented

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -17,7 +17,16 @@ func BlizzardOAuthClient(id, secret string) *battlenet.Client {
 		TokenURL:     endpoint.TokenURL,
 	}
 
+	// ctx := context.TODO()
+	// authClient := oauthCfg.Client(ctx)
+	// client := battlenet.NewClient("us", authClient)
+
+	// pool := x509.NewCertPool()
+	// pool.AppendCertsFromPEM(pemCerts)
+	// httpsClient := &http.Client{Transport: &http.Transport{TLSClientConfig: &tls.Config{RootCAs: pool}}}
+	// httpsClient := &http.Client{Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}}
 	ctx := context.TODO()
+	// ctx = context.WithValue(ctx, oauth2.HTTPClient, httpsClient)
 	authClient := oauthCfg.Client(ctx)
 	client := battlenet.NewClient("us", authClient)
 
